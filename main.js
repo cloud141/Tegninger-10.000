b32639352b88fa5f41c3003d6c6fe5cd7e5e6c3a
Bawar Hamid <bawar@live.dk>
2024-09-04 13:47:26 +0200

    roll again if all dices is rolled

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const playersAdded = [];
 const diceValues = [1, 2, 3, 4, 5, 6];
 let diceOnHold = [false, false, false, false, false, false];
-let heldDiceValues = [null, null, null, null, null, null];
+let heldDiceValues = ["", "", "", "", "", ""];
 
 function TriggerOnOfHold(dice) {
   let diceNumber = parseInt(dice.id.replace("dice", "")) - 1;
@@ -14,6 +14,36 @@ function TriggerOnOfHold(dice) {
     dice.style.opacity = "0.2";
     diceOnHold[diceNumber] = true;
     heldDiceValues[diceNumber] = diceValues[diceNumber];
+  }
+
+  if (diceOnHold.every((held) => held)) {
+    setTimeout(resetDiceHold, 1000);
+  }
+}
+
+function resetDiceHold() {
+  for (let i = 0; i < 6; i++) {
+    diceOnHold[i] = false;
+    document.getElementById("dice" + (i + 1)).style.opacity = "1";
+  }
+  rerollAllDice();
+}
+
+function rerollAllDice() {
+  const imgs = [
+    "imgs/dices1.png",
+    "imgs/dices2.png",
+    "imgs/dices3.png",
+    "imgs/dices4.png",
+    "imgs/dices5.png",
+    "imgs/dices6.png",
+  ];
+
+  for (let i = 1; i <= 6; i++) {
+    const randomIndex = Math.floor(Math.random() * imgs.length);
+    document.getElementById("dices" + i).src = imgs[randomIndex];
+    diceValues[i - 1] = randomIndex + 1;
+    heldDiceValues[i - 1] = diceValues[i - 1];
   }
 }
 
@@ -59,11 +89,12 @@ function rulTerning() {
     "imgs/dices5.png",
     "imgs/dices6.png",
   ];
-
+  console.table(heldDiceValues);
   for (let i = 1; i <= 6; i++) {
     // const randomIndex = Math.floor(Math.random() * imgs.length);
 
     // document.getElementById("dices" + i).src = imgs[randomIndex];
+
     if (!diceOnHold[i - 1]) {
       const randomIndex = Math.floor(Math.random() * imgs.length);
 
