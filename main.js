89dcf4fda5442b80ccc486cd4f5822acbe951851
Bawar Hamid <bawar@live.dk>
2024-09-04 10:43:42 +0200

    hi

--- main.js
+++ main.js
@@ -1,47 +1,8 @@
 const playersAdded = [];
 
-function addPlayers() {
-  const players = document.getElementById('playerId').value.trim();
-  playersAdded.push(players);
-  document.getElementById('display').innerText += players + '\n';
-  document.getElementById("playerId").value = "";
-}
-
-function deletePlayer() {
-  const players = document.getElementById('playerId').value.trim();
-
-  if (players !== "") {
-    const index = playersAdded.indexOf(players);
-
-    if (index > -1) {
-      playersAdded.splice(index, 1);
-      document.getElementById('display').innerText = playersAdded.join('\n');
-    }
-
-    document.getElementById("playerId").value = "";
-  }
-}
-
-element.addEventListener("click, addScore()");
-
-function addScore() {
-    let diceScore = document.getElementById("score");
-    if (diceScore + currentScore < 1000){
-        currentScore = 0;
-    }
-    else {
-        currentScore = diceScore + currentScore;
-    }   
-}
-
 let diceOnHold = [false, false, false, false, false, false];
-let diceValues = [0, 0, 0, 0, 0, 0];
-// var totalScore;
-// var roundScore = 0;
-
 function TriggerOnOfHold(dice) {
   let diceNumber = parseInt(dice.id.replace("dice", "")) - 1;
-  //   diceOnHold.push(dice.id);
 
   if (diceOnHold[diceNumber]) {
     dice.style.opacity = "1";
@@ -52,23 +13,52 @@ function TriggerOnOfHold(dice) {
   }
 }
 
-function rulTerning() {
+function addPlayers() {
+  const players = document.getElementById("playerId").value.trim();
+  playersAdded.push(players);
+  document.getElementById("display").innerText += players + "\n";
+  document.getElementById("playerId").value = "";
+}
 
-    const imgs = [
-        'imgs/dices1.png',
-        'imgs/dices2.png',
-        'imgs/dices3.png',
-        'imgs/dices4.png',
-        'imgs/dices5.png',
-        'imgs/dices6.png'
-    ];
+function deletePlayer() {
+  const players = document.getElementById("playerId").value.trim();
 
-    for (let i=1; i <= 6; i++) {
+  if (players !== "") {
+    const index = playersAdded.indexOf(players);
 
-        const randomIndex = Math.floor(Math.random() * imgs.length);
-
-        document.getElementById('dices' + i).src = imgs [randomIndex];
-
+    if (index > -1) {
+      playersAdded.splice(index, 1);
+      document.getElementById("display").innerText = playersAdded.join("\n");
     }
 
+    document.getElementById("playerId").value = "";
+  }
+}
+
+element.addEventListener("click, addScore()");
+
+function addScore() {
+  let diceScore = document.getElementById("score");
+  if (diceScore + currentScore < 1000) {
+    currentScore = 0;
+  } else {
+    currentScore = diceScore + currentScore;
+  }
+}
+
+function rulTerning() {
+  const imgs = [
+    "imgs/dices1.png",
+    "imgs/dices2.png",
+    "imgs/dices3.png",
+    "imgs/dices4.png",
+    "imgs/dices5.png",
+    "imgs/dices6.png",
+  ];
+
+  for (let i = 1; i <= 6; i++) {
+    const randomIndex = Math.floor(Math.random() * imgs.length);
+
+    document.getElementById("dices" + i).src = imgs[randomIndex];
+  }
 }
