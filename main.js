c6be0a02f283372ed74e826c15043182a93f04c2
StikkelMaal <msan37878@edu.ucl.dk>
2024-09-05 08:19:56 +0200

    khb

--- main.js
+++ main.js
@@ -35,30 +35,49 @@ function deletePlayer() {
   }
 }
 
-element.addEventListener("click, addScore()");
-
-function addScore() {
-  let diceScore = document.getElementById("score");
-  if (diceScore + currentScore < 1000) {
-    currentScore = 0;
-  } else {
-    currentScore = diceScore + currentScore;
-  }
-}
+let diceStats=[];
 
 function rulTerning() {
-  const imgs = [
-    "imgs/dices1.png",
-    "imgs/dices2.png",
-    "imgs/dices3.png",
-    "imgs/dices4.png",
-    "imgs/dices5.png",
-    "imgs/dices6.png",
+    const imgs = [
+        "imgs/dices1.png",
+        "imgs/dices2.png",
+        "imgs/dices3.png",
+        "imgs/dices4.png",
+        "imgs/dices5.png",
+        "imgs/dices6.png",
   ];
 
-  for (let i = 1; i <= 6; i++) {
-    const randomIndex = Math.floor(Math.random() * imgs.length);
+  diceStats = [];
 
-    document.getElementById("dices" + i).src = imgs[randomIndex];
-  }
+  for (let i = 1; i <= 6; i++) {
+      const randomIndex = Math.floor(Math.random() * imgs.length);
+      
+      document.getElementById("dices" + i).src = imgs[randomIndex];
+    }
+    diceStats.push(randomIndex);
 }
+
+console.log(diceStats);
+
+// element.addEventListener("click, addScore()");
+
+// function addScore() {
+//   let diceScore = document.getElementById("score");
+//   if (diceScore + currentScore < 1000) {
+//     currentScore = 0;
+//   } else {
+//     currentScore = diceScore + currentScore;
+//   }
+// }
+
+let currentRound 
+
+function checkForPoints() {
+    var diceOnHoldString = diceOnHold.toString(); 
+    if(diceOnHoldString == "1,2,3,4,5,6")
+        currentRound+=1000;
+    else if(diceOnHoldString == "1")
+        currentRound+=100; 
+    console.log(currentRound);
+}
+
